2e13eb77ec8ab6dee63f5584e9a1cf0ade7c684a
Joseph Weitekamp <jweite@amazon.com>
2019-05-10 18:06:48 -0400

    Changed build directory back to .aws-sam/build.

--- calc-lambda.py
+++ calc-lambda.py
@@ -10,16 +10,14 @@ def lambda_handler(event, context):
     if 'body' in event and isinstance(event['body'], str):
         event = json.loads(event['body'])
 
-    spl = re.split(r'([\+,\-])', event['formula'])
+    spl = re.split(r'(\+)', event['formula'])
     a = float(spl[0])
     op = spl[1]
     b = float(spl[2])
 
     retVal = {}
     if op == '+':
-       retVal['answer'] = a * b
-    elif op == '-':
-        retVal['answer'] = a - b
+       retVal['answer'] = a + b
     else:
        return {"statusCode": 500, "body": "Unknown operator in formula"}
  
