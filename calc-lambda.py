e5540086d458c1d3e18a30e8d22ad0aaebeb0124
Ubuntu <ubuntu@ip-172-31-88-124.ec2.internal>
2019-05-20 14:09:43 +0000

    Commenting out the import of ptvsd too, so we can demo the lambda locally without pip-ing in the package

--- calc-lambda.py
+++ calc-lambda.py
@@ -1,10 +1,10 @@
 import json
 import re
-import ptvsd
 
 # Enable ptvsd on 0.0.0.0 address and on port 5890 that we'll connect later with our IDE
-#ptvsd.enable_attach(address=('0.0.0.0', 5890), redirect_output=True)
-#ptvsd.wait_for_attach()
+  #import ptvsd
+  #ptvsd.enable_attach(address=('0.0.0.0', 5890), redirect_output=True)
+  #ptvsd.wait_for_attach()
 
 def lambda_handler(event, context):
     if 'body' in event and isinstance(event['body'], str):
